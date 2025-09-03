b645e0f5d6d88c6e637fe353285cb9681e733e37
Anil <anilmende2002@email.com>
2025-09-04 02:27:30 +0530

    v3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+// Components
+import MyNavbar from "./Components/Navbar";
 // Pages
 import RegisterPage from "./Pages/Register";
 import LoginPage from "./Pages/Login";
 // CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-
+import ListingPage from "./Pages/List";
 
 
 function App() {
   return (
+    <div>
+      <MyNavbar/>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />}/>
+        <Route path="/book/list" element={<ListingPage/>}/>
+      </Routes>
+    </div>
 
-    <Routes>
-      <Route path="/" element = {<h1>Home</h1>}/>
-      <Route path = "/login" element = {<LoginPage/>}/>
-      <Route path="/register" element={<RegisterPage/>}/>
-    </Routes>
   );
 }
 
